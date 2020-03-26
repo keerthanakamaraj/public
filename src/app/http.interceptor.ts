9331bbf34e17b87c3371a2210d400551e200be7f
arti.mishra <arti.mishra@intellectdesign.com>
2020-03-26 19:57:58 +0530

    Authorization token changes

--- src/app/http.interceptor.ts
+++ src/app/http.interceptor.ts
@@ -3,14 +3,24 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { ProvidehttpService } from "./providehttp.service";
 import { map } from "rxjs/operators";
+import { KeycloakService } from "keycloak-angular";
 
 @Injectable()
 export class HttpResponseInceptor implements HttpInterceptor {
-    constructor(private httpService: ProvidehttpService) {}
-
+    constructor(private httpService: ProvidehttpService, protected keycloakAngular: KeycloakService) {}
+    
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        const keycloakService = this.keycloakAngular.getKeycloakInstance();
+        
+               
+
+                if (keycloakService.token) {
+                    request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + keycloakService.token) });
+                }
         return next.handle(request).pipe(
             map((event: HttpEvent<any>) => {
+           
                 // if (event instanceof HttpResponse) {
                 //     if (event.body) {
                 //         this.httpService.checkForSession(event.body);
