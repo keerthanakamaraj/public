e0f7e342148e2f8b5b46875d66c60140150dcd04
Pratima <pratima.gupt@intellectdesign.com>
2020-03-28 16:36:56 +0530

    validation changes done by juhi

--- src/app/CustomerDtls/customer-handler.component.ts
+++ src/app/CustomerDtls/customer-handler.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CustomerDtlsComponent} from './CustomerDtls.component';
 import { FieldComponent } from '../field/field.component';
+import { RLOUIHandlerComponent } from 'src/app/rlouihandler/rlouihandler.component';
 
 @Component({
   selector: 'app-customer-handler',
   template: `<div style="display:none;"></div>`,
   styles: []
 })
-export class CustomerHandlerComponent implements OnInit {
+export class CustomerHandlerComponent extends RLOUIHandlerComponent implements OnInit {
 	@Input() MainComponent: CustomerDtlsComponent;
-	
+	formName: string = "CustomerDetails";
 	  ngOnInit() {
     // ngOnInit
   }
@@ -17,7 +18,8 @@ export class CustomerHandlerComponent implements OnInit {
   // OnFormLoad
   onFormLoad(arg0: {}) {
     console.log("customer .. On form load");
-    //this.MainComponent.CD_THIRD_NAME.setHidden(true);
+    super.onFormLoad({});
+    // this.MainComponent.CD_THIRD_NAME.setHidden(true);
 	}
 
  }
--- src/app/OccupationDtlsForm/occupation-handler.component.ts
+++ src/app/OccupationDtlsForm/occupation-handler.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { OccupationDtlsFormComponent} from './OccupationDtlsForm.component';
 import { FieldComponent } from '../field/field.component';
+import { RLOUIHandlerComponent } from 'src/app/rlouihandler/rlouihandler.component';
 
 @Component({
   selector: 'app-occupation-handler',
   template: `<div style="display:none;"></div>`,
   styles: []
 })
-export class OccupationHandlerComponent implements OnInit {
+export class OccupationHandlerComponent extends RLOUIHandlerComponent implements OnInit {
 	@Input() MainComponent: OccupationDtlsFormComponent;
-	
+	formName: string = "OccupationDetails";
 	  ngOnInit() {
     // ngOnInit
   }
@@ -17,6 +18,7 @@ export class OccupationHandlerComponent implements OnInit {
   // OnFormLoad
   onFormLoad(arg0: {}) {
     console.log("Occupation .. On form load");
+    super.onFormLoad({});
     //this.MainComponent.CD_THIRD_NAME.setHidden(true);
 	}
 
