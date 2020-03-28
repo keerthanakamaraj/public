f866899499bfc575521b0f8364bf4e6842347e6e
Vishal Kardode <vishal.kardode@intellectdesign.com>
2020-03-28 14:01:53 +0530

    DDE Changes - Added new Components in TAB

--- src/app/DDE/DDE.module.ts
+++ src/app/DDE/DDE.module.ts
@@ -7,13 +7,17 @@ import { FormsModule } from '@angular/forms';
 import { RAFormModule } from '../ra-form/ra-form.module';
 import { DDERoutingModule } from './DDE-routing.module';
 import { DDEComponent } from './DDE.component';
+import { VisitReportFormModule } from '../VisitReportForm/VisitReportForm.module';
 import { HeaderModule } from '../Header/Header.module';
 import { FamilyDetailsFormModule } from '../FamilyDetailsForm/FamilyDetailsForm.module';
 import { LiabilityDtlsFormModule } from '../LiabilityDtlsForm/LiabilityDtlsForm.module';
+import { NotepadDetailsFormModule } from '../NotepadDetailsForm/NotepadDetailsForm.module';
 import { IncomeSummaryFormModule } from '../IncomeSummaryForm/IncomeSummaryForm.module';
 import { CustomerDtlsModule } from '../CustomerDtls/CustomerDtls.module';
 import { OtherDeductionFormModule } from '../OtherDeductionForm/OtherDeductionForm.module';
 import { AssetDetailsFormModule } from '../AssetDetailsForm/AssetDetailsForm.module';
+import { GoNoGoModule } from '../go-no-go/go-no-go.module';
+import { DDEHandlerModule } from '../DDE/DDE-handler.module';
 @NgModule({
 imports: [
 CommonModule,
@@ -28,13 +32,17 @@ useFactory: HttpLoaderFactory,
 deps: [HttpClient]
 }
 }),
+VisitReportFormModule,
 HeaderModule,
 FamilyDetailsFormModule,
 LiabilityDtlsFormModule,
+NotepadDetailsFormModule,
 IncomeSummaryFormModule,
 CustomerDtlsModule,
 OtherDeductionFormModule,
 AssetDetailsFormModule,
+GoNoGoModule,
+DDEHandlerModule,
 ],
 declarations: [
 DDEComponent,
