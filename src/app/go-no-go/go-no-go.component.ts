ba3f25f2ac9addd702d941fa7b2c8bcdd9c6467e
Mohd Uvais Ansari <mohd.ansari@intellectdesign.com>
2020-03-31 19:06:22 +0530

    Go No Go save functionality added.

--- src/app/go-no-go/go-no-go.component.ts
+++ src/app/go-no-go/go-no-go.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
 import { ServiceStock } from '../service-stock.service';
 import { HttpResponse } from '@angular/common/http';
 
@@ -8,44 +8,46 @@ import { HttpResponse } from '@angular/common/http';
   styleUrls: ['./go-no-go.component.css']
 })
 export class GoNoGoComponent implements OnInit {
-  //apiResponse:any = response;
-  apiResponse:HttpResponse<any>;
+  
+  apiResponse: HttpResponse<any>;
   questionnaireCat: Map<Number, String> = new Map();
   answerCollection: Array<String> = new Array();
+  @ViewChildren('tbData') domRef: QueryList<ElementRef>;
 
-  constructor(private services: ServiceStock) { }
+  constructor(private services: ServiceStock, private renderer2: Renderer2) { }
 
   ngOnInit() {
-    this.loadQuestionnaireDtls();  
-    //this.afterLoadQuestionnaireDtls();
+    this.loadQuestionnaireDtls();
   }
 
   afterLoadQuestionnaireDtls() {
-    if(this.apiResponse) {
-      
+    this.answerCollection = [];
+    if (this.apiResponse) {
       this.apiResponse['GoNoGoDetails'].forEach((element) => {
-        if(element.AnswerSeq)
+        if (element.AnswerSeq)
           this.answerCollection.push(`${element.AnswerSeq}-${element.Remarks}`);
       });
 
-      console.log('AnswerCollection: ',this.answerCollection);
+      console.log('AnswerCollection: ', this.answerCollection);
 
       this.apiResponse['MstQuestionnaireDtls'].map((element) => {
         let tempAnsSeq: String = null;
-        
-        element['MstQuestionnaireAns'].every(answer => {
+
+        element['MstQuestionnaireAns'].forEach(answer => {
           tempAnsSeq = this.answerCollection.find(answers => answers.split('-')[0] === answer.AnswerSeq);
-          return false;
+          if(tempAnsSeq)
+            return false;
         });
 
-        if(tempAnsSeq != null &&  tempAnsSeq != undefined) {
+        if (tempAnsSeq != null && tempAnsSeq != undefined) {
           element.AnswerSeq = tempAnsSeq.split('-')[0];
           element.Remarks = tempAnsSeq.split('-')[1];
         }
+
         return element;
       });
     }
-    console.log(this.apiResponse);
+    console.log(this.apiResponse['MstQuestionnaireDtls']);
   }
 
   loadQuestionnaireDtls() {
@@ -54,9 +56,9 @@ export class GoNoGoComponent implements OnInit {
     inputMap.set('QueryParam.QuestionnaireCategory', 'go_no_go');
     inputMap.set('QueryParam.Product', 'PROD1');
     inputMap.set('QueryParam.SubProduct', 'SUBPROD1');
-    inputMap.set('QueryParam.ApplicationId', '222');
-     
-   this.services.http.fetchApi('/questionnaire', 'GET', inputMap).subscribe((httpResponse: HttpResponse<any>) => {
+    inputMap.set('QueryParam.ApplicationId', '666');
+
+    this.services.http.fetchApi('/questionnaire', 'GET', inputMap).subscribe((httpResponse: HttpResponse<any>) => {
       this.apiResponse = httpResponse.body;
       this.afterLoadQuestionnaireDtls();
     },
@@ -64,9 +66,79 @@ export class GoNoGoComponent implements OnInit {
         console.error(httpError);
         this.services.alert.showAlert(2, 'Something went wrong while fetching data!', -1);
       });
+  }
+
+  createSaveApiRequestBody(element, temp) {
+    if(temp['Remarks'] != undefined)
+      temp = {};
+
+    if(element.value != 'Select' && element.nodeName == 'SELECT') {
+      temp['AnswerSeq'] = element.value;
+      if(element.selectedOptions)
+        temp['Result'] = element.selectedOptions[0].text;
     }
 
-    persistData(e) {
-      console.log('Need to implement save functionality');
+    if(element.value != '' && element.nodeName == 'TEXTAREA') {
+      temp['Remarks'] = element.value;
     }
+
+    return temp;
+  }
+
+  persistData() {
+    console.log('Saving...');
+    
+    let temp = {};
+    const questionnaireArray = new Array();
+
+    this.domRef.forEach(element => {
+      temp = this.createSaveApiRequestBody(element.nativeElement, temp);
+      if(!this.isEmpty(temp) && 'AnswerSeq' in temp && 'Remarks' in temp) {
+          questionnaireArray.push(temp);
+      }
+    });
+
+    this.apiResponse['MstQuestionnaireDtls'].forEach(question => {
+      questionnaireArray.map(element => {
+        const answer = question['MstQuestionnaireAns'].find(answer => answer.AnswerSeq === element.AnswerSeq);
+
+        if(answer != undefined) {
+          element['QuestionSeq'] =  question.QuestionSeq;
+          element['ApplicationId'] =  '666';
+          element['QuestionnaireCategory'] =  question.QuestionnaireCategory;
+          element['CreatedBy'] =  sessionStorage.getItem('userId');
+          element['UpdatedBy'] =  sessionStorage.getItem('userId');
+        }
+        return element;
+      });
+
+    });
+
+    console.log('Questionnaire Array: ', questionnaireArray);
+    
+    let inputMap = new Map();
+    inputMap.clear();
+    inputMap.set('PathParam.ApplicationId', '666');
+    inputMap.set('Body.QuestionnaireDetails', questionnaireArray);
+    
+    this.services.http.fetchApi('/saveQuestionnaireDetails/{ApplicationId}', 'POST', inputMap).subscribe((httpResponse: HttpResponse<any>) => {
+      this.services.alert.showAlert(1, 'Record Saved Successfully!', 5000);
+      this.loadQuestionnaireDtls();
+    },
+      (httpError) => {
+        console.error(httpError);
+        this.services.alert.showAlert(2, 'Something went wrong while saving data!', -1);
+      });
+      
+  }
+
+  isEmpty(obj) {
+    for(var prop in obj) {
+      if(obj.hasOwnProperty(prop)) {
+        return false;
+      }
+    }
+  
+    return JSON.stringify(obj) === JSON.stringify({});
+  }
 }
