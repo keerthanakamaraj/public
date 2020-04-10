7d9caada3609bd3280e2595f832dbc2de6405020
Shweta-Pathari <Shweta.Pathari@intellectdesign.com>
2020-04-10 13:34:35 +0530

    customer details grid changes
    Template to display customer details by customer types created with file name customer-handler.component.html.
    grid tag from customerDtls.component.html removed and handler tag moved at top of the file.
    in customer-handler.component.ts API service call method to fetch customers by proposal id created.
    In customerDtls.component.ts , API method called which is available in handler. extra parameter passed in custDtlsEdit method.

--- src/app/CustomerDtls/customer-handler.component.ts
+++ src/app/CustomerDtls/customer-handler.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { CustomerDtlsComponent} from './CustomerDtls.component';
+import { Component, OnInit, ViewChild, Input, EventEmitter, Output } from '@angular/core';
+import { CustomerDtlsComponent } from './CustomerDtls.component';
 import { FieldComponent } from '../field/field.component';
 import { RLOUIHandlerComponent } from 'src/app/rlouihandler/rlouihandler.component';
-
+import { ServiceStock } from '../service-stock.service';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { ReadOnlyComponent } from '../rlo-ui-readonlyfield/rlo-ui-readonlyfield.component';
 @Component({
   selector: 'app-customer-handler',
-  template: `<div style="display:none;"></div>`,
-  styles: []
+  templateUrl: './customer-handler.component.html'
 })
 export class CustomerHandlerComponent extends RLOUIHandlerComponent implements OnInit {
-	@Input() MainComponent: CustomerDtlsComponent;
-	formName: string = "CustomerDetails";
-	  ngOnInit() {
+  @Output() custDtlsEdit: EventEmitter<any> = new EventEmitter<any>();
+  @Input() MainComponent: CustomerDtlsComponent;
+  formName: string = "CustomerDetails";
+
+  customerDetailsMap = new Map();
+  @ViewChild('"CD_CUSTOMER_TYPE"', { static: false }) CD_CUSTOMER_TYPE: ReadOnlyComponent;
+  @ViewChild('"CD_CUSTOMER_NAME"', { static: false }) CD_CUSTOMER_NAME: ReadOnlyComponent;
+  ngOnInit() {
     // ngOnInit
   }
 
@@ -19,10 +25,12 @@ export class CustomerHandlerComponent extends RLOUIHandlerComponent implements O
   onFormLoad(arg0: {}) {
     console.log("customer .. On form load");
     super.onFormLoad({});
+
+    // this.APIForCustomerData();
     this.MainComponent.CD_THIRD_NAME.setHidden(true);
     // this.MainComponent.CD_THIRD_NAME.setHidden(true);
   }
-  
+
   updateFullName(arg0: {}) {
     let fullName = "";
     if (this.MainComponent.CD_FIRST_NAME.getFieldValue()) {
@@ -41,5 +49,79 @@ export class CustomerHandlerComponent extends RLOUIHandlerComponent implements O
     this.MainComponent.CD_FULL_NAME.setValue(fullName);
   }
 
- }
+  async APIForCustomerData(event) {
+    let inputMap = new Map();
+    let custId: any = event.custSeq;
+    if (custId) {
+      inputMap.clear();
+      let criteriaJson: any = { "Offset": 1, "Count": 10, FilterCriteria: [] };
+      if (custId) {
+        criteriaJson.FilterCriteria.push({
+          "columnName": "ApplicationId",
+          "columnType": "String",
+          "conditions": {
+            "searchType": "equals",
+            "searchText": custId
+          }
+        });
+      }
+      inputMap.set('QueryParam.criteriaDetails', criteriaJson);
+      this.MainComponent.services.http.fetchApi('/BorrowerDetails', 'GET', inputMap).subscribe(
+        async (httpResponse: HttpResponse<any>) => {
+          var res = httpResponse.body;
+          var customerDataArr = [];
+          var BorrowerDetails = res['BorrowerDetails'];
+          if (BorrowerDetails) {
+            BorrowerDetails.forEach(eachBorrower => {
+              let customer = {};
+              customer['CustomerId'] = eachBorrower.BorrowerSeq;
+              customer['CD_CUSTOMER_TYPE'] = eachBorrower.CustomerType;
+              customer['CD_CUSTOMER_NAME'] = eachBorrower.FullName;
+              customerDataArr.push(customer);
+            });
+          }
+          this.apiSuccessCallback(customerDataArr);
+        },
+        async (httpError) => {
+          var err = httpError['error']
+          if (err != null && err['ErrorElementPath'] != undefined && err['ErrorDescription'] != undefined) {
+          }
+          this.MainComponent.services.alert.showAlert(2, 'Failed to load customer Borrower details!', -1);
+        }
+      );
+    }
+  }
+
+  apiSuccessCallback(customerDataArr: any[]) {
+    this.customerDetailsMap.clear();
+    customerDataArr.forEach(customer => {
+      if (customer != null && customer.CD_CUSTOMER_TYPE != undefined && customer != '') {
+        this.categoriseCustomers(customer.CD_CUSTOMER_TYPE, customer);
+      }
+    });
+  }
+  categoriseCustomers(customerType: String, customer: {}) {
+    let customerTypeArr = [];
+    if (customer !== null) {
+      if (this.customerDetailsMap.has(customerType)) {
+        customerTypeArr = this.customerDetailsMap.get(customerType);
+
+      }
+      else {
+        customerTypeArr = [];
+      }
+      customerTypeArr.push(customer);
+      this.customerDetailsMap.set(customerType, customerTypeArr);
+    }
+  }
+
+  public editCustomer(event, selectedCustomer) {
+    if (selectedCustomer) {
+      this.MainComponent.CUST_DTLS_GRID_custDtlsEdit(event, selectedCustomer.CustomerId);
+    }
+  }
+  doReset() {
+    this.MainComponent.onReset();
+  }
+}
 
