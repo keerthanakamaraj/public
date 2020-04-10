7146a9ca7e326c25c3869b402d98bd6b243ce6a8
Shweta-Pathari <Shweta.Pathari@intellectdesign.com>
2020-04-10 16:24:36 +0530

    customer details edit button changes
    edit button events and css handling methods added in customer-handler.component.ts

--- src/app/CustomerDtls/customer-handler.component.ts
+++ src/app/CustomerDtls/customer-handler.component.ts
@@ -116,12 +116,36 @@ export class CustomerHandlerComponent extends RLOUIHandlerComponent implements O
   }
 
   public editCustomer(event, selectedCustomer) {
-    if (selectedCustomer) {
+    if (selectedCustomer) {  
+      this.deactivateClasses();
+      event.target.classList.remove("fas");
+      event.target.classList.remove("fa-edit");
+      event.target.classList.add("customer-edit-active");
+      event.target.textContent = "Editing";
+     event.target.parentElement.classList.remove("customer-names");
+      event.target.parentElement.classList.add("customer-names-active");
       this.MainComponent.CUST_DTLS_GRID_custDtlsEdit(event, selectedCustomer.CustomerId);
     }
   }
   doReset() {
+    this.deactivateClasses();
     this.MainComponent.onReset();
   }
+deactivateClasses(){
+  let elementList= Array.from(document.getElementsByClassName("customer-names-active"));
+  elementList.forEach(element => {
+    element.classList.add("customer-names");
+    element.classList.remove("customer-names-active");
+  });  
+
+  elementList= Array.from(document.getElementsByClassName("customer-edit-active"));
+  elementList.forEach(element => {
+    element.classList.add("fas");
+    element.classList.add("fa-edit");
+    element.textContent = "";
+    element.classList.remove("customer-edit-active");
+  });
+}
+
 }
 
