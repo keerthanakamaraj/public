7bd195c170275993939c77b1b9bf868dfe414504
Vishal Kardode <vishal.kardode@intellectdesign.com>
2020-05-07 15:31:44 +0530

    Grid refresh issues fix- ECPV-1111

--- src/app/MyTrayForm/MyTrayForm.component.ts
+++ src/app/MyTrayForm/MyTrayForm.component.ts
@@ -20,123 +20,125 @@ import { MyTrayGridComponent } from '../MyTrayGrid/MyTrayGrid.component';
 const customCss: string = '';
 
 @Component({
-selector: 'app-MyTrayForm',
-templateUrl: './MyTrayForm.component.html'
+  selector: 'app-MyTrayForm',
+  templateUrl: './MyTrayForm.component.html'
 })
 export class MyTrayFormComponent extends FormComponent implements OnInit, AfterViewInit {
-@ViewChild('MT_SLIDER', {static: false}) MT_SLIDER: CheckBoxComponent;
-@ViewChild('MT_GRID', {static: false}) MT_GRID: MyTrayGridComponent;
-async revalidate(): Promise<number> {
-var totalErrors = 0;
-super.beforeRevalidate();
-await Promise.all([
-this.revalidateBasicField('MT_SLIDER'),
-]).then((errorCounts) => {
-errorCounts.forEach((errorCount)=>{
-totalErrors+=errorCount;
-});
-});
-this.errors = totalErrors;
-super.afterRevalidate();
-return totalErrors;
-}
-constructor(services: ServiceStock){
-super(services);
-this.value = new MyTrayFormModel();
-this.componentCode = 'MyTrayForm';
-this.displayBorder = false;
-}
-setReadOnly(readOnly){
-super.setBasicFieldsReadOnly(readOnly);
-}
-async onFormLoad(){
-this.setInputs(this.services.dataStore.getData(this.services.routing.currModal));
-this.MT_SLIDER.setValue(true);
-let inputMap = new Map();
-//this.MT_SLIDER.setValue('true');
-await this.MT_GRID.gridDataLoad({
-    'sliderVal': this.MT_SLIDER.getFieldValue(),
+  @ViewChild('MT_SLIDER', { static: false }) MT_SLIDER: CheckBoxComponent;
+  @ViewChild('MT_GRID', { static: false }) MT_GRID: MyTrayGridComponent;
+  async revalidate(): Promise<number> {
+    var totalErrors = 0;
+    super.beforeRevalidate();
+    await Promise.all([
+      this.revalidateBasicField('MT_SLIDER'),
+    ]).then((errorCounts) => {
+      errorCounts.forEach((errorCount) => {
+        totalErrors += errorCount;
+      });
     });
-    
-// await this.MT_GRID.gridDataLoad({
-// });
-// this.setDependencies();
- }
-setInputs(param : any){
-let params = this.services.http.mapToJson(param);
-if(params['mode']){
-this.mode = params['mode'];
-}
-}
-async submitForm(path, apiCode, serviceCode){
-this.submitData['formName'] = 'My Tray';
-await super.submit(path, apiCode, serviceCode);
-}
-getFieldInfo() {
-this.amountComponent.forEach(field => {this.additionalInfo[field.fieldID + '_desc'] = field.getFieldInfo();});
-this.comboFields.forEach(field => {this.additionalInfo[field.fieldID + '_desc'] = field.getFieldInfo();});
-this.fileUploadFields.forEach(field => {this.additionalInfo[field.fieldID + '_desc'] = field.getFieldInfo();});
-return this.additionalInfo;
-}
-getFieldValue(){
-return this.value;
-}
-setValue(inputValue, inputDesc=undefined) {
-this.setBasicFieldsValue(inputValue, inputDesc);
-this.value = new MyTrayFormModel();
-this.value.setValue(inputValue);
-this.setDependencies();
-this.passNewValue(this.value);
-}
-ngOnInit(){
-if(this.formCode == undefined) {this.formCode = 'MyTrayForm';}
-if(this.formOnLoadError){return;}
-var styleElement = document.createElement('style');
-styleElement.type = 'text/css';
-styleElement.innerHTML = customCss;
-styleElement.id = 'MyTrayForm_customCss';
-document.getElementsByTagName('head')[0].appendChild(styleElement);
-}
-ngOnDestroy(){
-this.unsubscribe$.next();
-this.unsubscribe$.complete();
-var styleElement = document.getElementById('MyTrayForm_customCss');
-styleElement.parentNode.removeChild(styleElement);
-}
-ngAfterViewInit(){
-setTimeout(() => {
-this.subsBFldsValueUpdates();
-this.onFormLoad();
-this.checkForHTabOverFlow();
-});
-}
-clearError(){
-super.clearBasicFieldsError();
-super.clearHTabErrors();
-super.clearVTabErrors();
-this.errors = 0;
-this.errorMessage = [];
-}
-onReset(){
-super.resetBasicFields();
-this.clearHTabErrors();
-this.clearVTabErrors();
-this.errors = 0;
-this.errorMessage = [];
-this.additionalInfo = undefined;
-this.dependencyMap.clear();
-this.value = new MyTrayFormModel();
-this.passNewValue(this.value);
-this.setReadOnly(false);
-this.onFormLoad();
-}
-async MT_SLIDER_change(event){
-let inputMap = new Map();
-await this.MT_GRID.gridDataLoad({
-'sliderVal': this.MT_SLIDER.getFieldValue(),
-});
-}
-fieldDependencies = {
-}
+    this.errors = totalErrors;
+    super.afterRevalidate();
+    return totalErrors;
+  }
+  constructor(services: ServiceStock) {
+    super(services);
+    this.value = new MyTrayFormModel();
+    this.componentCode = 'MyTrayForm';
+    this.displayBorder = false;
+  }
+  setReadOnly(readOnly) {
+    super.setBasicFieldsReadOnly(readOnly);
+  }
+  async onFormLoad() {
+    this.setInputs(this.services.dataStore.getData(this.services.routing.currModal));
+    this.MT_SLIDER.setValue(true);
+    let inputMap = new Map();
+    //this.MT_SLIDER.setValue('true');
+    await this.MT_GRID.gridDataLoad({
+      'sliderVal': this.MT_SLIDER.getFieldValue(),
+    });
+
+    // await this.MT_GRID.gridDataLoad({
+    // });
+    // this.setDependencies();
+  }
+  setInputs(param: any) {
+    let params = this.services.http.mapToJson(param);
+    if (params['mode']) {
+      this.mode = params['mode'];
+    }
+  }
+  async submitForm(path, apiCode, serviceCode) {
+    this.submitData['formName'] = 'My Tray';
+    await super.submit(path, apiCode, serviceCode);
+  }
+  getFieldInfo() {
+    this.amountComponent.forEach(field => { this.additionalInfo[field.fieldID + '_desc'] = field.getFieldInfo(); });
+    this.comboFields.forEach(field => { this.additionalInfo[field.fieldID + '_desc'] = field.getFieldInfo(); });
+    this.fileUploadFields.forEach(field => { this.additionalInfo[field.fieldID + '_desc'] = field.getFieldInfo(); });
+    return this.additionalInfo;
+  }
+  getFieldValue() {
+    return this.value;
+  }
+  setValue(inputValue, inputDesc = undefined) {
+    this.setBasicFieldsValue(inputValue, inputDesc);
+    this.value = new MyTrayFormModel();
+    this.value.setValue(inputValue);
+    this.setDependencies();
+    this.passNewValue(this.value);
+  }
+  ngOnInit() {
+    if (this.formCode == undefined) { this.formCode = 'MyTrayForm'; }
+    if (this.formOnLoadError) { return; }
+    var styleElement = document.createElement('style');
+    styleElement.type = 'text/css';
+    styleElement.innerHTML = customCss;
+    styleElement.id = 'MyTrayForm_customCss';
+    document.getElementsByTagName('head')[0].appendChild(styleElement);
+  }
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+    var styleElement = document.getElementById('MyTrayForm_customCss');
+    styleElement.parentNode.removeChild(styleElement);
+  }
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.subsBFldsValueUpdates();
+      this.onFormLoad();
+      this.checkForHTabOverFlow();
+    });
+  }
+  clearError() {
+    super.clearBasicFieldsError();
+    super.clearHTabErrors();
+    super.clearVTabErrors();
+    this.errors = 0;
+    this.errorMessage = [];
+  }
+  onReset() {
+    super.resetBasicFields();
+    this.clearHTabErrors();
+    this.clearVTabErrors();
+    this.errors = 0;
+    this.errorMessage = [];
+    this.additionalInfo = undefined;
+    this.dependencyMap.clear();
+    this.value = new MyTrayFormModel();
+    this.passNewValue(this.value);
+    this.setReadOnly(false);
+    this.onFormLoad();
+  }
+  async MT_SLIDER_change(event) {
+    let inputMap = new Map();
+    if (event) {
+      await this.MT_GRID.gridDataLoad({
+        'sliderVal': this.MT_SLIDER.getFieldValue()
+      });
+    }
+  }
+  fieldDependencies = {
+  }
 
 }
