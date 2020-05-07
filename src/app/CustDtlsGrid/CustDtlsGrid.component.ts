e7b3e28ac95c2f9f5f092354f4f64e2be023df6e
Pratima <pratima.gupt@intellectdesign.com>
2020-05-07 11:01:16 +0530

    bug fixing for Initiation stage

--- src/app/CustDtlsGrid/CustDtlsGrid.component.ts
+++ src/app/CustDtlsGrid/CustDtlsGrid.component.ts
@@ -12,89 +12,89 @@ const customCss: string = '';
   selector: 'app-CustDtlsGrid',
   templateUrl: './CustDtlsGrid.component.html',
   animations: [
-  trigger('slideInOut', [
-  state('false', style({ opacity: '0', overflow: 'hidden', height: '0px' })),
-  state('true', style({ height: '*', 'padding-top': '1rem' })),
-  transition('true => false', animate('300ms ease-out')),
-  transition('false => true', animate('300ms ease-in'))
-  ])
+    trigger('slideInOut', [
+      state('false', style({ opacity: '0', overflow: 'hidden', height: '0px' })),
+      state('true', style({ height: '*', 'padding-top': '1rem' })),
+      transition('true => false', animate('300ms ease-out')),
+      transition('false => true', animate('300ms ease-in'))
+    ])
   ],
 })
 export class CustDtlsGridComponent implements AfterViewInit {
-  constructor(private services: ServiceStock, private cdRef: ChangeDetectorRef) {}
-  @ViewChild('readonlyGrid', {static: true}) readonlyGrid: ReadonlyGridComponent;
-  
+  constructor(private services: ServiceStock, private cdRef: ChangeDetectorRef) { }
+  @ViewChild('readonlyGrid', { static: true }) readonlyGrid: ReadonlyGridComponent;
+
   @Output() ModifyCustDetails: EventEmitter<any> = new EventEmitter<any>();
   @Output() DeleteCustDetails: EventEmitter<any> = new EventEmitter<any>();
   @Input('formCode') formCode: string;
   @Input('displayTitle') displayTitle: boolean = true;
   @Input('displayToolbar') displayToolbar: boolean = true;
   @Input('fieldID') fieldID: string;
-  
+
   componentCode: string = 'CustDtlsGrid';
-  openedFilterForm:string = '';
-  hidden:boolean = false;
+  openedFilterForm: string = '';
+  hidden: boolean = false;
   gridConsts: any = {
     paginationPageSize: 5,
     gridCode: "CustDtlsGrid",
-    paginationReq:true
+    paginationReq: true
   };
-  columnDefs:any[] = [{
-    field:"CUST_TYPE_LBL",
-    width:22,
+  columnDefs: any[] = [{
+    field: "CUST_TYPE_LBL",
+    width: 22,
     resizable: true,
-    cellStyle: {'text-align': 'left'},
+    cellStyle: { 'text-align': 'left' },
     filter: false,
   },
   {
-    field:"FULL_NAME",
-    width:22,
+    field: "FULL_NAME",
+    width: 22,
     resizable: true,
-    cellStyle: {'text-align': 'left'},
+    cellStyle: { 'text-align': 'left' },
     filter: false,
   },
   {
-    field:"CIF",
-    width:22,
+    field: "CIF",
+    width: 22,
     resizable: true,
-    cellStyle: {'text-align': 'left'},
+    cellStyle: { 'text-align': 'left' },
     filter: false,
   },
   {
-    field:"DOB",
-    width:22,
+    field: "DOB",
+    width: 22,
     resizable: true,
-    cellStyle: {'text-align': 'left'},
+    cellStyle: { 'text-align': 'left' },
     filter: false,
   },
   {
-    width:6,
-    field:"",
+    width: 6,
+    field: "",
     sortable: false,
     filter: false,
     resizable: true,
     cellRenderer: 'buttonRenderer',
-    cellStyle: {'text-align': 'left'},
+    cellStyle: { 'text-align': 'left' },
     cellRendererParams: {
       gridCode: 'CustDtlsGrid',
       columnId: 'CD_Modify',
-    //   Type: '1',
+      //   Type: '1',
       CustomClass: 'btn-edit',
       onClick: this.CD_Modify_click.bind(this),
     },
   },
   {
-    width:6,
-    field:"",
+    width: 6,
+    field: "",
     sortable: false,
     filter: false,
     resizable: true,
     cellRenderer: 'buttonRenderer',
-    cellStyle: {'text-align': 'left'},
+    cellStyle: { 'text-align': 'left' },
     cellRendererParams: {
       gridCode: 'CustDtlsGrid',
       columnId: 'CD_Delete',
-    //   Type: '1',
+      //   Type: '1',
       CustomClass: 'btn-delete',
       onClick: this.CD_Delete_click.bind(this),
     },
@@ -103,18 +103,18 @@ export class CustDtlsGridComponent implements AfterViewInit {
   private unsubscribe$: Subject<any> = new Subject<any>();
   ngAfterViewInit() {
     this.services.translate.onLangChange
-    .pipe(takeUntil(this.unsubscribe$))
-    .subscribe((event: LangChangeEvent) => {
-      var colDefClone = [];
-      for(var i=0;i<this.columnDefs.length;i++){
-        colDefClone[i] = Object.assign({}, this.columnDefs[i]);
-      }
-      this.readonlyGrid.loadColums(colDefClone);
-    });
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((event: LangChangeEvent) => {
+        var colDefClone = [];
+        for (var i = 0; i < this.columnDefs.length; i++) {
+          colDefClone[i] = Object.assign({}, this.columnDefs[i]);
+        }
+        this.readonlyGrid.loadColums(colDefClone);
+      });
   }
-  onGridSizeChanged(){
+  onGridSizeChanged() {
     var colDefClone = [];
-    for(var i=0;i<this.columnDefs.length;i++){
+    for (var i = 0; i < this.columnDefs.length; i++) {
       colDefClone[i] = Object.assign({}, this.columnDefs[i]);
     }
     this.readonlyGrid.loadColums(colDefClone);
@@ -123,7 +123,7 @@ export class CustDtlsGridComponent implements AfterViewInit {
   }
   setColumnHidden(columnId: string, hidden: boolean) {
     Promise.all([this.readonlyGrid.gridLoadComplete.toPromise(), this.readonlyGrid.columnsInitialized.toPromise()])
-    .then(() => { this.readonlyGrid.gridColumnApi.setColumnVisible(columnId, !hidden); });
+      .then(() => { this.readonlyGrid.gridColumnApi.setColumnVisible(columnId, !hidden); });
   }
   isColumnHidden(columnId) {
     return !this.readonlyGrid.gridColumnApi.getColumn(columnId).isVisible();
@@ -135,7 +135,7 @@ export class CustDtlsGridComponent implements AfterViewInit {
     styleElement.id = 'CustDtlsGrid_customCss';
     document.getElementsByTagName('head')[0].appendChild(styleElement);
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
     var styleElement = document.getElementById('CustDtlsGrid_customCss');
@@ -144,30 +144,30 @@ export class CustDtlsGridComponent implements AfterViewInit {
   setValue(rowData) {
     this.readonlyGrid.setRowData(rowData);
   }
-  setHidden(value: boolean){
+  setHidden(value: boolean) {
     this.hidden = value;
   }
-  isHidden(){
+  isHidden() {
     return this.hidden;
   }
-  async CD_Modify_click(event){
+  async CD_Modify_click(event) {
     let inputMap = new Map();
     this.ModifyCustDetails.emit({
       'clickId': event.tempId,
     });
   }
-  async CD_Delete_click(event){
+  async CD_Delete_click(event) {
     let inputMap = new Map();
     this.DeleteCustDetails.emit({
       'clickId': event.tempId,
     });
   }
-  loadSpinner=false;
-  showSpinner(){
-    this.loadSpinner=true;
+  loadSpinner = false;
+  showSpinner() {
+    this.loadSpinner = true;
   }
-  hideSpinner(){
-    this.loadSpinner=false;
+  hideSpinner() {
+    this.loadSpinner = false;
   }
-  
+
 }
