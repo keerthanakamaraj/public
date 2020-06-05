0e9288f2815ce60f8259cf19d6bb37a4b75f4304
Vishal Kardode <vishal.kardode@intellectdesign.com>
2020-06-05 09:00:30 +0530

    Environment config fixes for dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,19 +3,19 @@ export const environment = {
   // baseURL: window.location.origin + '/olive/'
   baseURL: window.location.origin,
   // baseURL: 'http://10.11.12.53:9090',
-  authURL: 'https://iam.intellectqacloud.com/auth',
+  authURL: 'https://iam.intellectseecapps.com/auth',
   // realm: 'ecpvdev',
-  realm: window.location.hostname.split(".")[0],
+  realm: window.location.hostname.split('.')[0],
   clientId: 'fabric',
   // redirectURL: 'http://localhost:1841/#/home/LANDING'
-  redirectURL: window.location.hash.replace("#/","") == "" ? window.location.href + "#/home/LANDING" : window.location.href,
+  redirectURL: window.location.hash.replace('#/', '') === '' ? window.location.href + '#/home/LANDING' : window.location.href,
 
   serviceMap : {
-    "default" : "/initiation/publisher",
-    "/masters" : "/masters/publisher",
-    "/ui" : "/initiation/publisher",
-    "/los-wf" : "/los-wf/rest",
-    "/initiation": "/initiation/publisher",
-    "/rlo-de" : "/rlo-de/publisher"
+    'default' : '/initiation/publisher',
+    '/masters' : '/masters/publisher',
+    '/ui' : '/initiation/publisher',
+    '/los-wf' : '/los-wf/rest',
+    '/initiation': '/initiation/publisher',
+    '/rlo-de' : '/rlo-de/publisher'
   }
 };
