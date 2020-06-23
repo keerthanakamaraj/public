ad10ecd629a14cd93f44afcf9946b710a01af993
arti.mishra <arti.mishra@intellectdesign.com>
2020-06-23 16:06:56 +0530

    service map for interface api check eligibility

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   // baseURL: 'http://10.11.12.26:8180/olive/'
   // baseURL: "http://10.11.12.19:18180" //Mumbai
-    // baseURL: 'http://10.11.12.53:9090',
-   baseURL: 'https://rlfc.intellectseecapps.com',
+  //  baseURL: 'http://10.11.12.53:9090',
+  baseURL: 'https://rlfc.intellectseecapps.com',
   // baseURL: 'http://localhost:8081',
   enableKeycloak: false,
   authURL: 'https://iam.intellectseecapps.com/auth',
@@ -23,7 +23,8 @@ export const environment = {
   //   "/ui" : "/olive/publisher",
   //   "/los-wf" : "/los-wf/",
   //   "/initiation": "/olive/publisher",
-  //   "/rlo-de" : "/olive/publisher"
+  //   "/rlo-de" : "/olive/publisher",
+  //  "/los-integrator":"/los-integrator"
   // }
 
   serviceMap : {
@@ -32,7 +33,9 @@ export const environment = {
     "/ui" : "/initiation/publisher",
     "/los-wf" : "/los-wf/rest",
     "/initiation": "/initiation/publisher",
-    "/rlo-de" : "/rlo-de/publisher"
+    "/rlo-de" : "/rlo-de/publisher",
+     "/los-integrator":"/los-integrator"
+
   }
 
   // serviceMap : {
