662e4077eb53c1d405796e2bde2c8e4db5d6aea4
deepdalvi <deep.dalvi@intellectdesign.com>
2020-07-06 15:12:34 +0530

    minor changes commit before switch

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  cardConfig: any = {
-    customer360:{
+  @Input('cardMetaData') cardMetaData: string;
 
+  cardConfig: any = {
+    customer: {
+      class: 'col-6',
+      header: 'Customer 360 degrees',
+      isExpandable: true
     },
-    
+    interface: {
+      class: 'col-6',
+      header: 'Interface Results',
+      isExpandable: true
+    }
+
   }
 
   constructor() { }
--- src/app/under-writer/under-writer.component.ts
+++ src/app/under-writer/under-writer.component.ts
@@ -7,13 +7,53 @@ import{UWCustomerListComponent}from '../UWCustomerList/UWCustomerList.component'
 })
 export class UnderWriterComponent implements OnInit {
 
+  customizedJsonData = [
+    {
+      sectionName: "customer",
+      fullName: "Sulaiman Neville",
+      existingCustomer: true,
+      dob: "23 Mar 1991"
+    },
+    {
+      sectionName: "interfaceResults",
+      customer: [
+        {
+          name: "Vishal karan Kotwal",
+          internalResults: [
+            { watchOut: true }, { PAN: true }
+          ],
+          externalResults: [
+            { watchOut: true }, { PAN: true }
+          ],
+        },
+        {
+          name: "Darshan karan Kotwal",
+          internalResults: [
+            { watchOut: true }, { PAN: true }
+          ],
+          externalResults: [
+            { watchOut: true }, { PAN: true }
+          ],
+        }
+      ]
+    },
+    {
+      sectionName: "financialSummary",
+      totalIncome: 10000,
+      totalLiability: 10000,
+      totalAsstValue: 20000,
+    },
+    {
+      sectionName: "collateralDetails",
+      type: "house",
+      collateernalName: "",
+      amount: 20000,
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-
-  customizedJsonData = [
-    {}
-  ];
 
 }
