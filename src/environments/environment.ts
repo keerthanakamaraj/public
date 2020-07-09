d4954d2ad0990d7ea084d45b4dc1667d1476dfbb
Shweta-Pathari <Shweta.Pathari@intellectdesign.com>
2020-07-09 14:52:32 +0530

    DDE & QDE Customer Delete functionality
    Note-
    Cascade delete is working for QDE and DDE.
    On delete stage submit validation map update not handled yet.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   // baseURL: 'http://10.11.12.26:8180/olive/'
   // baseURL: "http://10.11.12.19:18180" //Mumbai
-  //  baseURL: 'http://10.11.12.53:9090',
-  baseURL: 'https://rlfc.intellectseecapps.com',
+    baseURL: 'http://10.11.12.53:9090',
+  //baseURL: 'https://rlfc.intellectseecapps.com',
   // baseURL: 'http://localhost:8081',
   enableKeycloak: false,
   authURL: 'https://iam.intellectseecapps.com/auth',
@@ -17,26 +17,26 @@ export const environment = {
   clientId: 'fabric',
   redirectURL: 'http://localhost:1841/#/home/LANDING',
 
-  // serviceMap : {
-  //   "default" : "/olive/publisher",
-  //   "/masters" : "/masters",
-  //   "/ui" : "/olive/publisher",
-  //   "/los-wf" : "/los-wf/",
-  //   "/initiation": "/olive/publisher",
-  //   "/rlo-de" : "/olive/publisher",
-  //  "/los-integrator":"/los-integrator"
-  // }
-
   serviceMap : {
-    "default" : "/initiation/publisher",
-    "/masters" : "/masters/publisher",
-    "/ui" : "/initiation/publisher",
-    "/los-wf" : "/los-wf/rest",
-    "/initiation": "/initiation/publisher",
-    "/rlo-de" : "/rlo-de/publisher",
-     "/los-integrator":"/los-integrator"
-
+    "default" : "/olive/publisher",
+    "/masters" : "/masters",
+    "/ui" : "/olive/publisher",
+    "/los-wf" : "/los-wf/",
+    "/initiation": "/olive/publisher",
+    "/rlo-de" : "/olive/publisher",
+   "/los-integrator":"/los-integrator"
   }
+
+  // serviceMap : {
+  //   "default" : "/initiation/publisher",
+  //   "/masters" : "/masters/publisher",
+  //   "/ui" : "/initiation/publisher",
+  //   "/los-wf" : "/los-wf/rest",
+  //   "/initiation": "/initiation/publisher",
+  //   "/rlo-de" : "/rlo-de/publisher",
+  //    "/los-integrator":"/los-integrator"
+
+  // }
 
   // serviceMap : {
   //   "default" : "/initiation/publisher",
