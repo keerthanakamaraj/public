3e8087a30d2a79559540a6ed50d540626c01be2b
Pratima <pratima.gupt@intellectdesign.com>
2020-08-11 17:31:58 +0530

    fixing for Demo Mobile no

--- src/app/rlo-ui-mobile/rlo-ui-mobile.component.ts
+++ src/app/rlo-ui-mobile/rlo-ui-mobile.component.ts
@@ -139,11 +139,11 @@ export class RloUiMobileComponent extends FieldComponent implements OnInit {
     var totalErrors: number = 0;
     //this.regex = "^([5]{1})([0-9]{7})*$";
     if(this.type == 'LandlineNo'){ 
-    this.regex = "^([246]{1})([0-9]{6})*$";
+    this.regex = "^[1-9][0-9]*$";
     // this.maxLengthValidation = 7
     }
     else{
-      this.regex = "^([5]{1})([0-9]{7})*$";
+      this.regex = "^[7][0-9]*$";
       // this.maxLengthValidation = 8
 
       // this.regex="^([7-9]{1})([0-9]+)*$";
