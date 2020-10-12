e272d436cfb547977e6ecb92206eec371be2de96
juhi-patil <juhi.patil@intellectdesign.com>
2020-10-12 17:59:47 +0530

    Gold Details UI changes with LOV changes done

--- src/app/GoldDetails/gold-handler.component.ts
+++ src/app/GoldDetails/gold-handler.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { GoldDetailsComponent} from './GoldDetails.component';
 import { FieldComponent } from '../field/field.component';
+import { RLOUIHandlerComponent } from '../rlouihandler/rlouihandler.component';
 
 @Component({
   selector: 'app-gold-handler',
   template: `<div style="display:none;"></div>`,
   styles: []
 })
-export class GoldDetailsHandlerComponent implements OnInit {
+export class GoldDetailsHandlerComponent extends RLOUIHandlerComponent implements OnInit {
 	@Input() MainComponent: GoldDetailsComponent;
   formName: string = "GoldDetails";
   
@@ -18,8 +19,10 @@ export class GoldDetailsHandlerComponent implements OnInit {
   // OnFormLoad
   onFormLoad(arg0: {}) {
     console.log("Gold .. On form load");
+    super.onFormLoad({});
     //this.MainComponent.CD_THIRD_NAME.setHidden(true);
-	}
+  }
+ 
 
  }
  
--- src/app/GoldDetailsGrid/GoldDetailsGrid.component.ts
+++ src/app/GoldDetailsGrid/GoldDetailsGrid.component.ts
@@ -23,7 +23,7 @@ const customCss: string = '';
 export class GoldDetailsGridComponent implements AfterViewInit {
     constructor(private services: ServiceStock, private cdRef: ChangeDetectorRef) { }
     @ViewChild('readonlyGrid', { static: true }) readonlyGrid: ReadonlyGridComponent;
-
+    @Output() ModifyGoldDeatils: EventEmitter<any> = new EventEmitter<any>();
     @Input('formCode') formCode: string;
     @Input('displayTitle') displayTitle: boolean = true;
     @Input('displayToolbar') displayToolbar: boolean = true;
@@ -39,34 +39,41 @@ export class GoldDetailsGridComponent implements AfterViewInit {
     };
     columnDefs: any[] = [{
         field: "GoldOrnamentType",
-        width: 22,
-        cellStyle: { 'text-align': 'left' },
-        filter: "agTextColumnFilter",
-        filterParams: {
-            suppressAndOrCondition: true,
-            applyButton: true,
-            clearButton: true,
-            filterOptions: ["contains"],
-            caseSensitive: true,
-        },
-    },
-    {
-        field: "Weight",
-        width: 22,
+        width: 15,
         cellStyle: { 'text-align': 'left' },
         filter: false,
     },
     {
-        field: "Value",
-        width: 22,
+        field: "Count",
+        width: 15,
+        cellStyle: { 'text-align': 'left' },
+        filter: false,
+    },
+    {
+        field: "Weight",
+        width: 15,
+        cellStyle: { 'text-align': 'left' },
+        filter: false,
+    },
+    {
+        field: "TotalWeight",
+        width: 15,
         cellStyle: { 'text-align': 'left' },
         filter: false,
     },
     {
         field: "MarketRate",
-        width: 22,
-        cellStyle: { 'text-align': 'left' },
+        width: 14,
+        cellStyle: { 'text-align': 'right' },
         filter: false,
+        valueFormatter: this.formatAmount.bind(this),
+    },
+    {
+        field: "Value",
+        width: 14,
+        cellStyle: { 'text-align': 'right' },
+        filter: false,
+        valueFormatter: this.formatAmount.bind(this),
     },
     {
         width: 6,
@@ -82,6 +89,7 @@ export class GoldDetailsGridComponent implements AfterViewInit {
             Type: '1',
             CustomClass: 'btn-edit',
             IconClass: 'fas fa-edit fa-lg',
+            onClick: this.GD_MODIFY_click.bind(this),
         },
     },
     {
@@ -98,6 +106,7 @@ export class GoldDetailsGridComponent implements AfterViewInit {
             Type: '1',
             CustomClass: 'btn-delete',
             IconClass: 'fa fa-trash fa-lg',
+            onClick: this.GD_DELETE_click.bind(this),
         },
     },
     ];
@@ -130,6 +139,7 @@ export class GoldDetailsGridComponent implements AfterViewInit {
         return !this.readonlyGrid.gridColumnApi.getColumn(columnId).isVisible();
     }
     ngOnInit(): void {
+        this.readonlyGrid.setGridDataAPI(this.gridDataAPI.bind(this));
         var styleElement = document.createElement('style');
         styleElement.type = 'text/css';
         styleElement.innerHTML = customCss;
@@ -145,11 +155,123 @@ export class GoldDetailsGridComponent implements AfterViewInit {
     setValue(rowData) {
         this.readonlyGrid.setRowData(rowData);
     }
+    gridDataLoad(formInputs) {
+        this.readonlyGrid.setFormInputs(formInputs);
+    }
+    refreshGrid() {
+        this.readonlyGrid.refreshGrid();
+    }
     setHidden(value: boolean) {
         this.hidden = value;
     }
     isHidden() {
         return this.hidden;
+    }
+    async gridDataAPI(params, gridReqMap: Map<string, any>, event) {
+        let inputMap = new Map();
+        inputMap.clear();
+        let applicationId: any = event.ApplicationId;
+        let criteriaJson: any = { "Offset": 1, "Count": 10, FilterCriteria: [] };
+        if (applicationId) {
+            criteriaJson.FilterCriteria.push({
+                "columnName": "ApplicationId",
+                "columnType": "String",
+                "conditions": {
+                    "searchType": "equals",
+                    "searchText": applicationId
+                }
+            }); inputMap.set('QueryParam.criteriaDetails.FilterCriteria', criteriaJson.FilterCriteria);
+        }
+        if (gridReqMap.get("FilterCriteria")) {
+            var obj = gridReqMap.get("FilterCriteria");
+            for (var i = 0; i < obj.length; i++) {
+                switch (obj[i].columnName) {
+                    case "GoldDetailSeq": obj[i].columnName = "GoldDetailSeq"; break;
+                    case "Weight": obj[i].columnName = "Body.GoldDetails.Weight"; break;
+                    case "Count": obj[i].columnName = "Body.GoldDetails.Count"; break;
+                    case "TotalWeight": obj[i].columnName = "Body.GoldDetails.TotalWeight"; break;
+                    case "Value": obj[i].columnName = "Body.GoldDetails.Value"; break;
+                    case "MarketRate": obj[i].columnName = "Body.GoldDetails.MarketRate"; break;
+                    case "GoldOrnamentType": obj[i].columnName = "Body.GoldDetails.GoldOrnamentType"; break;
+                    default: console.error("Column ID '" + obj[i].columnName + "' not mapped with any key");
+                }
+            }
+        }
+        if (gridReqMap.get("OrderCriteria")) {
+            var obj = gridReqMap.get("OrderCriteria");
+            for (var i = 0; i < obj.length; i++) {
+                switch (obj[i].columnName) {
+                    case "GoldDetailSeq": obj[i].columnName = "GoldDetailSeq"; break;
+                    case "Weight": obj[i].columnName = "Body.GoldDetails.Weight"; break;
+                    case "Count": obj[i].columnName = "Body.GoldDetails.Count"; break;
+                    case "TotalWeight": obj[i].columnName = "Body.GoldDetails.TotalWeight"; break;
+                    case "Value": obj[i].columnName = "Body.GoldDetails.Value"; break;
+                    case "MarketRate": obj[i].columnName = "Body.GoldDetails.MarketRate"; break;
+                    case "GoldOrnamentType": obj[i].columnName = "Body.GoldDetails.GoldOrnamentType"; break;
+                    default: console.error("Column ID '" + obj[i].columnName + "' not mapped with any key");
+                }
+            }
+        }
+        this.readonlyGrid.combineMaps(gridReqMap, inputMap);
+        this.services.http.fetchApi('/GoldDetails', 'GET', inputMap).subscribe(
+            async (httpResponse: HttpResponse<any>) => {
+                var res = httpResponse.body;
+                var loopDataVar10 = [];
+                var loopVar10 = res['GoldDetails'];
+                if (loopVar10) {
+                    for (var i = 0; i < loopVar10.length; i++) {
+                        var tempObj = {};
+                        tempObj['GoldDetailSeq'] = loopVar10[i].GoldDetailSeq;
+                        tempObj['Weight'] = loopVar10[i].Weight;
+                        tempObj['Count'] = loopVar10[i].Count;
+                        tempObj['TotalWeight'] = loopVar10[i].TotalWeight;
+                        tempObj['Value'] = loopVar10[i].Value;
+                        tempObj['MarketRate'] = loopVar10[i].MarketRate;
+                        tempObj['GoldOrnamentType'] = loopVar10[i].GoldOrnamentType.text;
+                        loopDataVar10.push(tempObj);
+                    }
+                }
+                this.readonlyGrid.apiSuccessCallback(params, loopDataVar10);
+            },
+            async (httpError) => {
+                var err = httpError['error']
+                if (err != null && err['ErrorElementPath'] != undefined && err['ErrorDescription'] != undefined) {
+                }
+            }
+        );
+
+    }
+    async GD_MODIFY_click(event) {
+        let inputMap = new Map();
+        const selectedData0 = this.readonlyGrid.getSelectedData();
+        this.ModifyGoldDeatils.emit({
+            'GoldKey': event['GoldDetailSeq'],
+        });
+
+    }
+    async GD_DELETE_click(event) {
+        let inputMap = new Map();
+        inputMap.clear();
+        inputMap.set('PathParam.ApplicationId', event.GoldDetailSeq);
+        if (confirm("Are you sure you want to Delete?")) {
+            this.services.http.fetchApi('/GoldDetails/{ApplicationId}', 'DELETE', inputMap, '/rlo-de').subscribe(
+                async (httpResponse: HttpResponse<any>) => {
+                    var res = httpResponse.body;
+                    this.services.alert.showAlert(1, 'rlo.success.delete.gold', 5000);
+
+                    // if (this.familyDetails.length == 1)
+                    //     this.services.rloCommonData.updateValuesFundLineGraph("remove");
+
+                    this.readonlyGrid.refreshGrid();
+                },
+                async (httpError) => {
+                    var err = httpError['error']
+                    if (err != null && err['ErrorElementPath'] != undefined && err['ErrorDescription'] != undefined) {
+                    }
+                    this.services.alert.showAlert(2, 'rlo.error.delete.gold', -1);
+                }
+            );
+        }
     }
     loadSpinner = false;
     showSpinner() {
@@ -158,5 +280,14 @@ export class GoldDetailsGridComponent implements AfterViewInit {
     hideSpinner() {
         this.loadSpinner = false;
     }
+
+    formatAmount(number) {
+        if (number.value) {
+            return this.services.formatAmount(number.value, null, null,false);
+        } else {
+            return '-';
+        }
+    }
+    
 
 }
