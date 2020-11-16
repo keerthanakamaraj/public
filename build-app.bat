d9d4648cc472fe0e82406a1d678908110a7ff767
Vishal Kardode <vishal.kardode@intellectdesign.com>
2020-11-16 23:45:21 +0530

    Build Batch

---
+++ build-app.bat
@@ -0,0 +1,33 @@
+@echo off
+REM Build Angular App 
+
+setlocal
+
+    echo Build Options
+    echo 1. Cloud Build ( default )
+    echo 2. Demo Server Build
+    echo 3. Canara Bank IUT Build
+
+    set _option=1
+    set /p _option="Select Option from above "
+
+    @REM echo Selected : %_option%
+
+    set config=production
+
+    IF %_option% == 2 (
+        set config=demo
+    ) ELSE (
+        IF %_option% == 3 (
+            set config=canara-iut
+        ) ELSE (
+            set config=production
+        )
+    )
+
+    echo selected config %config%
+    echo running command ng build --configuration %config% --base-href /rlo/ --sourceMap=true
+
+    ng build --configuration %config% --base-href /rlo/ --sourceMap=true
+
+endlocal
