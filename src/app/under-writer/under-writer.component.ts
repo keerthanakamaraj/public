f7889e6c209f95a20fb095a3847684d0f894af98
Shweta-Pathari <Shweta.Pathari@intellectdesign.com>
2021-01-04 19:57:28 +0530

    Hardcoded removed

--- src/app/under-writer/under-writer.component.ts
+++ src/app/under-writer/under-writer.component.ts
@@ -335,7 +335,7 @@ export class UnderWriterComponent extends FormComponent implements OnInit {
     this.instanceId = this.services.dataStore.getRouteParam(this.services.routing.currModal, 'instanceId');
     this.userId = this.services.dataStore.getRouteParam(this.services.routing.currModal, 'userId');
 
-    this.applicationId = 5587; //4424
+    //this.applicationId = 5587; //4424
 
     if (this.userId === undefined || this.userId == '') {
       this.claimTask(this.taskId);
