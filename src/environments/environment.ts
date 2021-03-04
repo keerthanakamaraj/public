00c5379fdb74c2e48e6faa83b311863f1a21b7fa
deepdalvi <deep.dalvi@intellectdesign.com>
2021-03-04 16:07:20 +0530

    PMS:environment ts change.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,12 +8,12 @@ export const environment = {
   production: false,
   // baseURL: 'http://10.11.12.26:8180/olive/'
   // baseURL: "http://10.11.12.19:18180" //Mumbai
-  //baseURL: 'http://10.11.12.53:9090', // LOCAL
+  baseURL: 'http://10.11.12.53:9090', // LOCAL
   // baseURL: 'https://rlfc.intellectseecapps.com', //LIVE
-    baseURL: 'http://10.11.10.42:20052',
-     enableKeycloak: true,
+  // baseURL: 'http://10.11.10.42:20052',
+  enableKeycloak: true,
   //  authURL: 'https://iam.intellectseecapps.com/auth',
-   authURL: 'http://10.11.12.15:18080/auth',
+  authURL: 'http://10.11.12.15:18080/auth',
   //authURL: 'https://iam.intellectqacloud.com/auth', // QA
   //  realm: 'ecpvdev',
   // realm: 'dabank', // QA
@@ -25,31 +25,31 @@ export const environment = {
   arxLogoutURL: 'http://tcldevapp173.intellectdesign.com:10751/ARXAuth/AALLogout.jsp',
 
   //DIT
-  // serviceMap : {
-  //   "default" : "/olive/publisher",
-  //   "/masters" : "/masters",
-  //   "/ui" : "/olive/publisher",
-  //   "/los-wf" : "/los-wf/",
-  //   "/initiation": "/olive/publisher",
-  //   "/rlo-de" : "/olive/publisher",
-  //  "/los-integrator":"/los-integrator",
-  //   "/lettermangement" :"/lettermangement/publisher",
-  //   "los-verification":"/los-verification",
-  //   "/common-de": "/los-verification"
-  // }
+  serviceMap: {
+    "default": "/olive/publisher",
+    "/masters": "/masters",
+    "/ui": "/olive/publisher",
+    "/los-wf": "/los-wf/",
+    "/initiation": "/olive/publisher",
+    "/rlo-de": "/olive/publisher",
+    "/los-integrator": "/los-integrator",
+    "/lettermangement": "/lettermangement/publisher",
+    "los-verification": "/los-verification",
+    "/common-de": "/los-verification"
+  }
 
   //Canara
-  serviceMap: {
-    "default": "/olive/publisher/rlo-initiation",
-    "/masters": "/olive/publisher/rlo-masters",
-    "/ui": "/olive/publisher/rlo-initiation",
-    "/los-wf": "/los-wf/rest",
-    "/initiation": "/olive/publisher/rlo-initiation",
-    "/rlo-de": "/olive/publisher/rlo-data-enrich",
-    "/los-integrator": "/los-integrator",
-    "/lettermangement" :"/olive/publisher/rlo-letter-mgmt",
-    "/common-de": "/olive/publisher/rlo-common-de"
-  }
+  // serviceMap: {
+  //   "default": "/olive/publisher/rlo-initiation",
+  //   "/masters": "/olive/publisher/rlo-masters",
+  //   "/ui": "/olive/publisher/rlo-initiation",
+  //   "/los-wf": "/los-wf/rest",
+  //   "/initiation": "/olive/publisher/rlo-initiation",
+  //   "/rlo-de": "/olive/publisher/rlo-data-enrich",
+  //   "/los-integrator": "/los-integrator",
+  //   "/lettermangement": "/olive/publisher/rlo-letter-mgmt",
+  //   "/common-de": "/olive/publisher/rlo-common-de"
+  // }
 
   //IUT
   // serviceMap: {
@@ -81,4 +81,3 @@ export const environment = {
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
 
-        
