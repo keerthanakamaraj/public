f9174fce2c230478c015e4098a9f79ca6083c4c5
Vishal Kardode <vishal.kardode@intellectdesign.com>
2021-03-11 17:37:59 +0530

    corrected env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,10 +5,13 @@ export const environment = {
   // baseURL: 'http://10.11.12.53:9090',
   authURL: 'https://iam.intellectseecapps.com/auth',
   // realm: 'ecpvdev',
+  enableKeycloak: true,
   realm: window.location.hostname.split('.')[0],
   clientId: 'fabric',
   // redirectURL: 'http://localhost:1841/#/home/LANDING'
   redirectURL: window.location.hash.replace('#/', '') === '' ? window.location.href + '#/home/LANDING' : window.location.href,
+  arxAuthURL: 'http://tcldevapp173.intellectdesign.com:10751/ARXAL/AALIndex.jsp',
+  arxLogoutURL: 'http://tcldevapp173.intellectdesign.com:10751/ARXAuth/AALLogout.jsp',
 
   serviceMap : {
     'default' : '/initiation/publisher',
@@ -16,6 +19,10 @@ export const environment = {
     '/ui' : '/initiation/publisher',
     '/los-wf' : '/los-wf/rest',
     '/initiation': '/initiation/publisher',
-    '/rlo-de' : '/rlo-de/publisher'
+    '/rlo-de' : '/rlo-de/publisher',
+    '/los-integrator': '/los-integrator',
+    '/lettermangement': '/lettermangement/publisher',
+    'los-verification': '/los-verification',
+    '/common-de': '/common-de/publisher'
   }
 };
