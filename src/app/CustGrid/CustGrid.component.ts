d0d431dac17c57b050a850e4ae0a9d8236948a82
juhi-patil <juhi.patil@intellectdesign.com>
2021-03-17 18:54:19 +0530

    operation customer grid changes

--- src/app/CustGrid/CustGrid.component.ts
+++ src/app/CustGrid/CustGrid.component.ts
@@ -12,18 +12,18 @@ const customCss: string = '';
   selector: 'app-CustGrid',
   templateUrl: './CustGrid.component.html',
   animations: [
-  trigger('slideInOut', [
-  state('false', style({ opacity: '0', overflow: 'hidden', height: '0px' })),
-  state('true', style({ height: '*', 'padding-top': '1rem' })),
-  transition('true => false', animate('300ms ease-out')),
-  transition('false => true', animate('300ms ease-in'))
-  ])
+    trigger('slideInOut', [
+      state('false', style({ opacity: '0', overflow: 'hidden', height: '0px' })),
+      state('true', style({ height: '*', 'padding-top': '1rem' })),
+      transition('true => false', animate('300ms ease-out')),
+      transition('false => true', animate('300ms ease-in'))
+    ])
   ],
 })
 export class CustGridComponent implements AfterViewInit {
-  constructor(private services: ServiceStock, private cdRef: ChangeDetectorRef) {}
-  @ViewChild('readonlyGrid', {static: true}) readonlyGrid: ReadonlyGridComponent;
-  
+  constructor(private services: ServiceStock, private cdRef: ChangeDetectorRef) { }
+  @ViewChild('readonlyGrid', { static: true }) readonlyGrid: ReadonlyGridComponent;
+
   @Input('formCode') formCode: string;
   @Input('displayTitle') displayTitle: boolean = true;
   @Input('displayToolbar') displayToolbar: boolean = true;
@@ -31,57 +31,57 @@ export class CustGridComponent implements AfterViewInit {
   @Input() ApplicationId: string = undefined;
 
   componentCode: string = 'CustGrid';
-  openedFilterForm:string = '';
-  hidden:boolean = false;
+  openedFilterForm: string = '';
+  hidden: boolean = false;
   gridConsts: any = {
     paginationPageSize: 10,
     gridCode: "CustGrid",
-    paginationReq:false
+    paginationReq: false
   };
-  columnDefs:any[] = [{
-    field:"Cust_Type",
-    width:25,
+  columnDefs: any[] = [{
+    field: "Cust_Type",
+    width: 25,
     sortable: false,
     resizable: true,
-    cellStyle: {'text-align': 'left'},
+    cellStyle: { 'text-align': 'left' },
   },
   {
-    field:"Cust_Name",
-    width:25,
+    field: "Cust_Name",
+    width: 25,
     sortable: false,
     resizable: true,
-    cellStyle: {'text-align': 'left'},
+    cellStyle: { 'text-align': 'left' },
   },
   {
-    field:"Cust_ID",
-    width:25,
+    field: "Cust_ID",
+    width: 25,
     sortable: true,
     resizable: true,
-    cellStyle: {'text-align': 'left'},
+    cellStyle: { 'text-align': 'left' },
   },
   {
-    field:"Cust_DOB",
-    width:25,
+    field: "Cust_DOB",
+    width: 25,
     sortable: true,
     resizable: true,
-    cellStyle: {'text-align': 'left'},
+    cellStyle: { 'text-align': 'left' },
   },
   ];
   private unsubscribe$: Subject<any> = new Subject<any>();
   ngAfterViewInit() {
     this.services.translate.onLangChange
-    .pipe(takeUntil(this.unsubscribe$))
-    .subscribe((event: LangChangeEvent) => {
-      var colDefClone = [];
-      for(var i=0;i<this.columnDefs.length;i++){
-        colDefClone[i] = Object.assign({}, this.columnDefs[i]);
-      }
-      this.readonlyGrid.loadColums(colDefClone);
-    });
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((event: LangChangeEvent) => {
+        var colDefClone = [];
+        for (var i = 0; i < this.columnDefs.length; i++) {
+          colDefClone[i] = Object.assign({}, this.columnDefs[i]);
+        }
+        this.readonlyGrid.loadColums(colDefClone);
+      });
   }
-  onGridSizeChanged(){
+  onGridSizeChanged() {
     var colDefClone = [];
-    for(var i=0;i<this.columnDefs.length;i++){
+    for (var i = 0; i < this.columnDefs.length; i++) {
       colDefClone[i] = Object.assign({}, this.columnDefs[i]);
     }
     this.readonlyGrid.loadColums(colDefClone);
@@ -90,7 +90,7 @@ export class CustGridComponent implements AfterViewInit {
   }
   setColumnHidden(columnId: string, hidden: boolean) {
     Promise.all([this.readonlyGrid.gridLoadComplete.toPromise(), this.readonlyGrid.columnsInitialized.toPromise()])
-    .then(() => { this.readonlyGrid.gridColumnApi.setColumnVisible(columnId, !hidden); });
+      .then(() => { this.readonlyGrid.gridColumnApi.setColumnVisible(columnId, !hidden); });
   }
   isColumnHidden(columnId) {
     return !this.readonlyGrid.gridColumnApi.getColumn(columnId).isVisible();
@@ -103,7 +103,7 @@ export class CustGridComponent implements AfterViewInit {
     styleElement.id = 'CustGrid_customCss';
     document.getElementsByTagName('head')[0].appendChild(styleElement);
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
     var styleElement = document.getElementById('CustGrid_customCss');
@@ -112,84 +112,102 @@ export class CustGridComponent implements AfterViewInit {
   gridDataLoad(formInputs) {
     this.readonlyGrid.setFormInputs(formInputs);
   }
-  refreshGrid(){
+  refreshGrid() {
     this.readonlyGrid.refreshGrid();
   }
-  setHidden(value: boolean){
+  setHidden(value: boolean) {
     this.hidden = value;
   }
-  isHidden(){
+  isHidden() {
     return this.hidden;
   }
-  async gridDataAPI(params, gridReqMap: Map<string, any>, event){
+  async gridDataAPI(params, gridReqMap: Map<string, any>, event) {
     let inputMap = new Map();
-		inputMap.clear();
-		let custId:any = event.passCustGrid;
-		let criteriaJson: any = { "Offset": 1, "Count": 10, FilterCriteria: [] };
-		if (custId) {
-			criteriaJson.FilterCriteria.push({
-				"columnName": "ApplicationId",
-				"columnType": "String",
-				"conditions": {
-					"searchType": "equals",
-					"searchText": custId
-				}
+    inputMap.clear();
+    let custId: any = event.passCustGrid;
+    let criteriaJson: any = { "Offset": 1, "Count": 10, FilterCriteria: [] };
+    if (custId) {
+      criteriaJson.FilterCriteria.push({
+        "columnName": "ApplicationId",
+        "columnType": "String",
+        "conditions": {
+          "searchType": "equals",
+          "searchText": custId
+        }
       });
-		}
-		inputMap.set('QueryParam.criteriaDetails', criteriaJson);
-    this.services.http.fetchApi('/BorrowerDetails', 'GET', inputMap, '/rlo-de').subscribe(
-    async (httpResponse: HttpResponse<any>) => {
-      var res = httpResponse.body;
-      var loopDataVar4 = [];
-      let BorrowerDetail = res['BorrowerDetails'];
-      var loopVar4 = res['BorrowerDetails'];
-      var loopVar4 = BorrowerDetail.filter(function (BorrowerDetail) {
-        return BorrowerDetail.CustomerType !== 'R' && BorrowerDetail.CustomerType !== 'F';
-      });
-      console.log("BorrowerDetails", loopVar4);
-      if (loopVar4) {
-        for (var i = 0; i < loopVar4.length; i++) {
-          var tempObj = {};
-          if(this.services.rloCommonData.globalApplicationDtls.CustomerType == 'I' || loopVar4[i].CustomerType == 'A' ){
-          tempObj['Cust_Name'] = loopVar4[i].FullName;
-          tempObj['Cust_DOB'] = loopVar4[i].DOB;
-          tempObj['Cust_ID'] = loopVar4[i].ICIFNumber; 
-          if(loopVar4[i].CustomerType == 'B'){
-          tempObj['Cust_Type'] = 'Primary'
-          }
-          if(loopVar4[i].CustomerType == 'A'){
-            tempObj['Cust_Type'] = 'Add On'
-            }
-          }
-          else if(this.services.rloCommonData.globalApplicationDtls.CustomerType == 'C'){
-            tempObj['Cust_Name'] = loopVar4[i].RegisteredName;
-          tempObj['Cust_DOB'] = loopVar4[i].DateOfIncorporation;
-          tempObj['Cust_ID'] = loopVar4[i].ICIFNumber; 
-          if(loopVar4[i].CustomerType == 'B'){
-          tempObj['Cust_Type'] = 'Corporate'
-          }
-          if(loopVar4[i].CustomerType == 'A'){
-            tempObj['Cust_Type'] = 'Member'
-            }
-          }
-        loopDataVar4.push(tempObj);}
-      }
-      this.readonlyGrid.apiSuccessCallback(params, loopDataVar4);
-    },
-    async (httpError)=>{
-      var err = httpError['error']
-      if(err!=null && err['ErrorElementPath'] != undefined && err['ErrorDescription']!=undefined){
-      }
-      this.services.alert.showAlert(2, 'failed', -1);
     }
+    inputMap.set('QueryParam.criteriaDetails', criteriaJson);
+    this.services.http.fetchApi('/BorrowerDetails', 'GET', inputMap, '/rlo-de').subscribe(
+      async (httpResponse: HttpResponse<any>) => {
+        var res = httpResponse.body;
+        var loopDataVar4 = [];
+        let BorrowerDetail = res['BorrowerDetails'];
+        var loopVar4 = res['BorrowerDetails'];
+        var loopVar4 = BorrowerDetail.filter(function (BorrowerDetail) {
+          return BorrowerDetail.CustomerType !== 'R' && BorrowerDetail.CustomerType !== 'F';
+        });
+        console.log("BorrowerDetails", loopVar4);
+        if (loopVar4) {
+          for (var i = 0; i < loopVar4.length; i++) {
+            var tempObj = {};
+            if (this.services.rloCommonData.globalApplicationDtls.CustomerType == 'I' || loopVar4[i].CustomerType == 'A') {
+              tempObj['Cust_Name'] = loopVar4[i].FullName;
+              tempObj['Cust_DOB'] = loopVar4[i].DOB;
+              tempObj['Cust_ID'] = loopVar4[i].ICIFNumber;
+              if (loopVar4[i].CustomerType == 'B') {
+                tempObj['Cust_Type'] = 'Primary'
+              }
+              if (loopVar4[i].CustomerType == 'A') {
+                tempObj['Cust_Type'] = 'Add On'
+              }
+            }
+            else if (this.services.rloCommonData.globalApplicationDtls.CustomerType == 'C') {
+              tempObj['Cust_Name'] = loopVar4[i].RegisteredName;
+              tempObj['Cust_DOB'] = loopVar4[i].DateOfIncorporation;
+              tempObj['Cust_ID'] = loopVar4[i].ICIFNumber;
+              if (loopVar4[i].CustomerType == 'B') {
+                tempObj['Cust_Type'] = 'Corporate'
+              }
+              if (loopVar4[i].CustomerType == 'A') {
+                tempObj['Cust_Type'] = 'Member'
+              }
+            }
+            else {
+              tempObj['Cust_Name'] = loopVar4[i].FullName;
+              tempObj['Cust_DOB'] = loopVar4[i].DOB;
+              tempObj['Cust_ID'] = loopVar4[i].ICIFNumber;
+              if (loopVar4[i].CustomerType == 'B') {
+                tempObj['Cust_Type'] = 'Borrower'
+              }
+              else if (loopVar4[i].CustomerType == 'CB') {
+                tempObj['Cust_Type'] = 'Co Borrower'
+              }
+              else if (loopVar4[i].CustomerType == 'G') {
+                tempObj['Cust_Type'] = 'Guarantor'
+              }
+              else {
+                tempObj['Cust_Type'] = 'Co Borrower'
+              }
+            }
+            loopDataVar4.push(tempObj);
+          }
+        }
+        this.readonlyGrid.apiSuccessCallback(params, loopDataVar4);
+      },
+      async (httpError) => {
+        var err = httpError['error']
+        if (err != null && err['ErrorElementPath'] != undefined && err['ErrorDescription'] != undefined) {
+        }
+        this.services.alert.showAlert(2, 'failed', -1);
+      }
     );
-    
+
   }
-  loadSpinner=false;
-  showSpinner(){
-    this.loadSpinner=true;
+  loadSpinner = false;
+  showSpinner() {
+    this.loadSpinner = true;
   }
-  hideSpinner(){
-    this.loadSpinner=false;
+  hideSpinner() {
+    this.loadSpinner = false;
   }
 }
